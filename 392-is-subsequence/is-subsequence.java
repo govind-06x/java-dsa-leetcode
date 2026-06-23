221a91d662a29e5756a1e81ed2039c2d639c568a
Govinda Cheruvupalli <govinda.cheruvupalli@gmail.com>
2026-06-23 07:32:07 +0530

    Time: 2 ms (71.49%) | Memory: 43 MB (11.87%) - LeetSync

--- 392-is-subsequence/is-subsequence.java
+++ 392-is-subsequence/is-subsequence.java
@@ -1,6 +1,9 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
         int i = 0;
+        if (s.length() > t.length()) {
+            return false;
+}
         for (int j = 0; j < t.length() && i<s.length(); j++) {
             if(s.charAt(i) == t.charAt(j)){
                 i++;
