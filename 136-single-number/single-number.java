04732591172d9c408c4e5151e9c4ff22273182f6
Govinda Cheruvupalli <govinda.cheruvupalli@gmail.com>
2026-07-08 17:04:34 +0530

    Time: 14 ms (15.33%) | Memory: 46.9 MB (64.85%) - LeetSync

---
+++ 136-single-number/single-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        
+        HashMap<Integer,Integer > map = new HashMap<>();
+        for (int i : nums) {
+            map.put(i, map.getOrDefault(i, 0)+1);
+        }
+        for (int i : map.keySet()) {
+            if (map.get(i) == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
