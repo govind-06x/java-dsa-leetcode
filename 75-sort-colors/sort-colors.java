233840db08db125c14ba2c9caddeedc6bedb4c64
Govinda Cheruvupalli <govinda.cheruvupalli@gmail.com>
2026-08-23 21:17:17 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (97.61%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,5 +1,24 @@
 class Solution {
     public void sortColors(int[] nums) {
-        Arrays.sort(nums);
+        int zeroCount = 0, oneCount = 0, twoCont = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                zeroCount++;
+            } else if (nums[i] == 1) {
+                oneCount++;
+            } else {
+                twoCont++;
+            }
+        }
+        oneCount += zeroCount;
+        twoCont += oneCount;
+        change(nums,0,zeroCount,0);
+        change(nums,zeroCount,oneCount,1);
+        change(nums,oneCount,twoCont,2);
+    }
+     private static void change(int[] nums, int st, int end, int val) {
+        for (int j2 = st; j2 < end; j2++) {
+            nums[j2] = val;
+        }
     }
 }
